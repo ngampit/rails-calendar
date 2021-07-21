2d6cc14ff57d771a7790615170e240c6b0e206e1
ngampit <ngampitt@yahoo.com>
2021-07-21 16:49:54 -0700

    UI finish draft

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,19 @@
 
 ActiveRecord::Schema.define(version: 2021_07_21_203930) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "meetings", force: :cascade do |t|
     t.string "name"
     t.datetime "start_time"
     t.datetime "end_time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "searches", force: :cascade do |t|
+    t.datetime "start_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
